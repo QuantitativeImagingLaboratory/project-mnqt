c1387bfaa1daa154013aeec8b12926354ec7c64f
QaemParasla <32251524+QaemParasla@users.noreply.github.com>
2017-12-05 18:17:12 -0800

    Update Interpolation.py
    Fixed: now it scales with different height and width

--- Interpolation.py
+++ Interpolation.py
@@ -16,16 +16,25 @@ class Interpolation:
         return the f(unknown) or intentity at unknown"""
 
         # Write your code for linear interpolation here
-        # pt1 (leftx,pix)
-        # pt2 (rightx,pix)
+        #leftPix(30)----------unkownPix(?)---------------------rightPix(45)
+        #leftX:2    ----------   dx:4     ---------------------rightX:8
+
+        #(30*(8-4)) + (45*(4-2))  = 35
+        #  (8-2)        (8-2)
+
+        # pt1[leftX,pix]
+        # pt2[rightX,pix]
         # unknown = dx
 
-        if (pt1[0] != pt2[0]):
-            fpart = pt1[1] * ((pt2[0] - unknown) / (pt2[0] - pt1[0]))
-            spart = pt2[1] * ((unknown - pt1[0]) / (pt2[0] - pt1[0]))
+        leftX, leftPix = pt1
+        rightX, rightPix = pt2
+
+        if (leftX != rightX):
+            fpart = leftPix * ((rightX - unknown) / (rightX - leftX))
+            spart = rightPix * ((unknown - leftX) / (rightX - leftX))
             pix = fpart + spart
-        else:
-            pix = pt1[1]
+        else: #This usually occurs when you are at last column of matrix (because we dont have pix to the right)
+            pix = leftPix #leftPix will equal rightPix so use any of the two
 
         return pix
 
@@ -43,23 +52,39 @@ class Interpolation:
         # Write your code for bilinear interpolation here
         # May be you can reuse or call linear interpolatio method to compute this task
 
-        #pt1(leftx, q11, q12)
-        #pt2(rightx, q21, q22)
-        #pt3(bottomy)
-        #pt4(topy)
 
-        if (pt2[0] - pt1[0]) != 0:
-            r1 = self.linear_interpolation((pt1[0],pt1[1]),(pt2[0],pt2[1]),unknown[0])
-            r2 = self.linear_interpolation((pt1[0],pt1[2]),(pt2[0],pt2[2]), unknown[0])
-        else:
-            r1 = pt1[1]
-            r2 = pt1[2]
 
-        if (pt3 - pt4) != 0:
-            avgPix = self.linear_interpolation((pt4,r2), (pt3,r1), unknown[1])
+        #                    _C O L S__
+        #              leftX    dX    rightX
+        # R      topY| q11      r1    q21
+        # O        dY|          P
+        # W          |
+        # S   bottomY| q12      r2    q22
+
+        #pt1[leftX, q11, q12]
+        #pt2[rightX, q21, q22]
+        #pt3[topY]
+        #pt4[bottomY]
+        #unknown[dy,dx]
+
+        leftX, q11, q12 = pt1
+        rightX, q21, q22 = pt2
+
+        topY = pt3
+        bottomY = pt4
+
+        dy, dx = unknown
+
+        if (rightX - leftX) != 0:
+            r1 = self.linear_interpolation((leftX,q11),(rightX,q21), dx)
+            r2 = self.linear_interpolation((leftX,q12),(rightX,q22), dx)
         else:
-            avgPix = (int(pt1[1]) + int(pt1[2]) + int(pt2[1]) + int(pt2[2]))/4
+            r1 = q11
+            r2 = q12
+
+        if (bottomY - topY) != 0:
+            avgPix = self.linear_interpolation((bottomY,r2), (topY,r1), dy)
+        else:
+            avgPix = (int(q11) + int(q12) + int(q21) + int(q22))/4
 
         return avgPix
-
-
